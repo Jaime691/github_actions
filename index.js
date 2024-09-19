1504a2791e3d7f595758757e0f5adaeffaebe7b4
Jaime691 <jmayorgadev@gmail.com>
2024-09-19 16:16:23 -0500

    fix data

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ exports.handler = async ( event ) => {
     return {
         statusCode: 200,
         body: JSON.stringify( {
-            message: data.choices[ 0 ].message.content
+            message: data
         } )
     };
 };
